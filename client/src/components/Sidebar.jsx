ef5da31b7cb25fadbfdb535f2f4a3a70cf36615d
shubhamraj82 <raj.shubh8216@gmail.com>
2025-10-10 10:17:14 +0530

    home page ready

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import {useNavigate} from 'react-router-dom';
+import assets from '../assets/assets';
 
-const Sidebar = () => {
+const Sidebar = ({selectedUser,setSelectedUser}) => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>Sidebar</h1>
+    <div className={`bg-[#8185B2]/10 h-full p-5 rounded-l-xl overflow-y-scroll text-white ${selectedUser ? "max-md:hidden" : ""} `}>
+    <div className='pb-5'>
+      <div className='flex justify-between items-center'>
+        <img src={assets.logo} alt="logo" className='max-w-40'/>
+        <div className='relative py-2 group'>
+          <img src={assets.menu_icon} alt="Menu" className='max-h-5 cursor-pointer'/>
+          <div className='absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282142] border border-gray-600 text-gray-100 hidden group-hover:block'>
+            <p onClick={()=>navigate('/profile')} className='cursor-pointer text-sm'>Edit Profile</p>
+            <hr className='my-2 border-t border-gray-500'/>
+            <p className='cursor-pointer text-sm'>Logout</p>
+          </div>
+        </div>
+      </div>
+    </div> 
     </div>
   )
 }
