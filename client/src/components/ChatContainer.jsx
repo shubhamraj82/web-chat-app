61a2d717ff9f42db4d8cf3e87c5f38ab75db29fb
shubhamraj82 <raj.shubh8216@gmail.com>
2025-10-11 11:45:01 +0530

    chatarea completed

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
-import assets from '../assets/assets'
+import React, { useEffect, useRef } from 'react'
+import assets, { messagesDummyData } from '../assets/assets'
+import { formatDate } from '../lib/utilis'
 
 const ChatContainer = ({selectedUser,setSelectedUser}) => {
+
+  const scrollEnd= useRef()
+  useEffect(()=>{
+    if(scrollEnd.current){
+      scrollEnd.current.scrollIntoView({behavior:"smooth"})
+    }
+  },[])
+
   return selectedUser ? (
     <div className='h-full overflow-scroll relative backdrop-blur-lg'>
       {/* //HEADER PART */}
@@ -16,8 +25,33 @@ const ChatContainer = ({selectedUser,setSelectedUser}) => {
       </div>
 
       {/* // CHAT AREA */}
-      <div>
-        
+      <div className='flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6'>
+        {messagesDummyData.map((msg,index)=>(
+          <div key={index} className={`flex items-end gap-2 justify-end ${msg.senderId!=='680f50e4f10f3cd28382ecf9' && 'flex-row-reverse'}`}>
+            {msg.image ? (
+              <img src={msg.image} alt='' className='max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8'/>
+            ):(
+              <p className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${msg.senderId === '680f50e4f10f3cd28382ecf9' ? 'rounded-br-none' : 'rounded-bl-none'}`}>{msg.text}</p>
+            )}
+            <div className='text-center text-xs'>
+              <img src={msg.senderId === '680f50e4f10f3cd28382ecf9' ? assets.avatar_icon : assets.profile_martin} alt="" className='w-7 rounded-full'/>
+              <p className='text-gray-500'>{formatDate(msg.createdAt)}</p>
+            </div>
+          </div>
+        ))}
+        <div ref={scrollEnd}></div>
+      </div>
+
+      {/* BOTTOM AREA */}
+      <div className='absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3'>
+        <div className='flex-1 flex items-center bg-gray-100/12 ox-3 rounded-full'>
+          <input type="text" placeholder='Send a message...' className='flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400'/>
+          <input type="file" id='image' accept='image/png, image/jpeg' hidden/>
+          <label htmlFor="image">
+            <img src={assets.gallery_icon} alt="" className='w-5 mr-2 cursor-pointer'/>
+          </label>
+        </div>
+        <img src={assets.send_button} alt="" className='w-7 cursor-pointer'/>
       </div>
     </div>
   ) : (
