2a86eb5e9ff37efce1e8d73c7a39b4f16290f327
shubhamraj82 <raj.shubh8216@gmail.com>
2025-10-22 15:27:03 +0530

    message part backend done

--- server/controller/messageController.js
+++ server/controller/messageController.js
@@ -1,5 +1,7 @@
 import Message from "../models/Message.js";
 import User from "../models/user.js";
+import cloudinary  from "../lib/cloudinary.js";
+import {io, userSocketMap} from "../server.js";
 
 //get all user except logged in user for sidebar
 export const getUserForSidebar=async(req,res)=>{
@@ -20,5 +22,68 @@ export const getUserForSidebar=async(req,res)=>{
     } catch (error) {
         console.log(error.message);
         res.json({success:false, message:error.message})
+    }
+}
+
+ // get all messages for selected user
+ export const getMessages=async(req,res)=>{
+    try {
+        const {id:selectedUserId}=req.params;
+        const myId = req.user._id;
+        const messages=await Message.find({
+            $or:[
+                {senderId:myId, receiverId:selectedUserId},
+                {senderId:selectedUserId, receiverId:myId},
+            ]
+        })
+        await Message.updateMany({senderId:selectedUserId,receiverId:myId},
+            {seen:true});
+            res.json({success:true, messages})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false, message:error.message})
+    }
+ }
+
+ // api to mark as message as seen usng message id
+ export const markMessageAsSeen=async(req,res)=>{
+    try {
+        const {id} =req.params;
+        await Message.findByIdAndUpdate(id, {seen:true})
+        res.json({success:true})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false,message:error.message})
+    }
+ }
+
+//send message to selected user
+export const sendMessage = async (req,res)=>{
+    try {
+        const { text , image}=req.params;
+        const receiverId=req.params.id;
+        const senderId=req.user._id;
+
+        let imageUrl;
+        if(image){
+            const uploadResponse = await cloudinary.uploader.upload(image)
+            imageUrl = uploadResponse.secure_url;
+        }
+        const newMessage=await Message.create({
+            senderId,
+            receiverId,
+            text,
+            image:imageUrl
+        })
+
+        //emit the new message to the receiver's  socket
+        const receiverSocketId=userSocketMap[receiverId];
+        if(receiverSocketId){
+            io.to(receiverSocketId).emit("newMessage",newMessage)
+        }
+        res.json({success:true,newMessage});
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false,message:error.message})
     }
 }
--- server/server.js
+++ server/server.js
@@ -4,10 +4,38 @@ import "dotenv/config";
 import http from "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
+import {Server} from "socket.io";
 
 // Create HTTP server and express app
 const app= express();
 const server = http.createServer(app);
+
+// Initialize socket.io
+export const io = new Server(server,{
+    cors:{origin:"*"}
+})
+
+// store online users
+export const userSocketMap={}; // {userId:socketId}
+
+// socket.io connection handler
+io.on("connection", (socket)=>{
+    const userId=socket.handshake.query.userId;
+    console.log("User Connected ", userId);
+
+    if(userId) userSocketMap[userId]=socket.id;
+
+    // Emit online users to all connected clients
+    io.emit("getOnlineUsers",Object.keys(userSocketMap));
+    
+    socket.on("disconnect", ()=>{
+        console.log("User Disconnected", userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    })
+})
+
 
 //Middleware setup
 app.use(express.json({limit:"10mb"}));
@@ -16,6 +44,7 @@ app.use(cors());
 //Routes setup
 app.use("/api/status",(req,res)=> res.send("server is running..."));
 app.use("/api/auth",userRouter);
+app.use("/api/messages",messageRouter);
 
 
 //connect to database
