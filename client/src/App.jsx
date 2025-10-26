59ab8838cd9b87a23c20f5e8990828f69cccf407
shubhamraj82 <raj.shubh8216@gmail.com>
2025-10-27 00:28:45 +0530

    all setups done for deployment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import { AuthContext } from '../context/AuthContext'
 const App = () => {
   const {authUser}=useContext(AuthContext)
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+    <div className="bg-[url('/bgImage.svg')] bg-contain">
       <Routes>
         <Route path='/' element={authUser ?<HomePage /> : <Navigate to="/login" />} />
         <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
--- server/server.js
+++ server/server.js
@@ -50,5 +50,8 @@ app.use("/api/messages",messageRouter);
 //connect to database
 await connectDB();
 
+if(process.env.NODE_ENV!=="production"){
 const PORT = process.env.PORT || 5000;
 server.listen(PORT,()=> console.log("Server is running on port " + PORT));
+}
+ export default server;
